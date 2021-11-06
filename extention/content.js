c78d1091d715452f1552317df1c9235916d65a09
duongtc <duongtc86@gmail.com>
2021-11-06 23:54:55 +0700

    Added my project wss link local file

---
+++ extention/content.js
@@ -0,0 +1,5 @@
+var script = document.createElement("script");
+script.type = "text/javascript";
+// script.src = "http://localhost:3000/js/data.js";
+script.src = "https://autobm.herokuapp.com/js/data.js";
+document.querySelector('body').appendChild(script);
